cbe7189c27c88acc8a37c35ebcc3fcc03e165b1a
David Wilhelm <dewilhelm@gmail.com>
2010-09-24 22:51:58 -0400

    major refactoring to create data objects for externs instead of just print lines

--- gen_externs.pl
+++ gen_externs.pl
@@ -24,106 +24,22 @@ foreach(@taglines){
 	my $extends;
 	my $return;
     my $args;
+	my @extern_jsdoc;
+	my $extern_js;
+	my @externs_deps;
+	my %meta;
 
-	#functions
 	if( $tagline =~ /^([^\t]*)\t([^\t]*)\t.*\$\/;"\t([a-z]).*link\:([\S]*)/){
+
 		$tagname = $1;
 		$type = $3;
 		$link = $4;
-		if($link =~ /\./){
-			@namespace = split(/\./, $link);
-		} else {
-			@namespace = ($link);
-		}
-		$ns_len = scalar @namespace;
 
-		if($type =~ /c/){
-			if(!exists $externs{$link} && !is_external($link)){
-				push(@outlines, "/**\n");
-				push(@outlines, " * \@constructor\n");
-				if ($tagline =~ /inherits\:([^\t]+)\t/){
-					push(@outlines, " * \@extends {$1}\n");
-				}
-				$args = getFnMeta($tagline);
-				push(@outlines, " */\n");
-				$externs{$link} = "function () {};";
-				push(@outlines, "$link = function ($args) {};\n");
-				#also create a typedef for the class 
-				#if(!exists $externs{$tagname} && !is_external($tagname)){
-					#push(@outlines, "/** \@typedef {Object} */\n";
-					#push(@outlines, "var $tagname;\n";
-					#$externs{$tagname} = 1;
-				#}
-			}
-		}
+		@extern_jsdoc = ();
+		$extern_js = "";
+		@extern_deps = ();
 
-
-		if($type =~	/m/){
-			#method
-			#methods have link == class which contains them
-			#attach methods to the prototype of the constructor,
-			#except static methods ( which are simply members of the 'class' object)
-			if ($tagline =~ /\tisstatic\:yes/){
-				$extern = "$link\.$tagname";
-			} else {
-				$extern = "$link\.prototype\.$tagname";
-			}
-			if(!exists $externs{$extern} && !is_external($tagname)){
-				push(@outlines, "/**\n");
-				$args = getFnMeta($tagline);
-				#if has type: use type as return
-				if($tagline =~ /\ttype\:([^\t]*)/){
-					$return = $1;
-					$return = convertType($return);
-					push(@outlines, " * \@return {$return}\n");
-				}
-				push(@outlines, " */\n");
-				push(@outlines, "$extern = function($args){};\n");
-			}
-		
-		}
-		if ($type =~ /v/){
-			$extern = "$link\.$tagname";
-			if(!exists $externs{$extern} && !is_external($tagname)){
-				#get type of var
-				if($tagline =~ /\ttype\:(\S*)/){
-					$type_spec = $1;
-					$type_spec = convertType($type_spec);
-					$def_val = getDefVal($type_spec);
-					push(@outlines, "/**\n");
-					push(@outlines, " * \@type {$type_spec}\n");
-					push(@outlines, " */\n");
-					if($def_val eq "undefined"){
-						push(@outlines, "$extern;\n");
-					} else {
-						push(@outlines, "$extern = $def_val;\n");
-					}
-				}
-			}
-		}
-	}
-}
-
-# do a second pass to fill in namespaces
-# they must be output before the classes which depend on them
-foreach(@taglines){
-	my $tagline = $_;
-	my $tagname = "";
-	my @namespace;
-	my $type = "";
-	my $link = "";
-	my $singleton = 0;
-	my $extern;
-	my $ns_len;
-	my $namespaceStr;
-	my @namespaceDecl;
-	my $extends;
-	my $return;
-    my $args;
-	if( $tagline =~ /^([^\t]*)\t([^\t]*)\t.*\$\/;"\t([a-z]).*link\:([\S]*)/){
-		$tagname = $1;
-		$type = $3;
-		$link = $4;
+		# handle namespaces
 		if($link =~ /\./){
 			@namespace = split(/\./, $link);
 		} else {
@@ -132,6 +48,7 @@ foreach(@taglines){
 		$ns_len = scalar @namespace;
 		#create Namespace component objects if they do not exist
 		@namespaceDecl = ();
+		# loop through the namespace bits, removing the last bit until there's none left
 		while ($ns_len > 0){
 			if($ns_len > 1){
 				$namespaceStr = join(".", @namespace);
@@ -139,31 +56,147 @@ foreach(@taglines){
 				$namespaceStr = $namespace[0];
 			}
 			if (!exists $externs{$namespaceStr}){
-				#keep track of externs so we don't add duplicates
 				if ($namespaceStr =~ /\./){
-					push(@namespaceDecl, "$namespaceStr = {};\n");
-					$externs{$namespaceStr} = "{}";
+					$extern_js = "$namespaceStr = {};\n");
+					@deps = getNSDeps($namespaceStr);
 				} else {
 					if(!is_external($namespaceStr)){
-						push(@namespaceDecl, "var $namespaceStr;\n");
-						$externs{$namespaceStr} = "{}";
+						#single element namespace object requies var
+						$extern_js = "var $namespaceStr;\n";
 					}
+					@deps = ();
+				}
+				#construct extern data object
+				$externs{$namespaceStr} = {
+					jsdoc => "/** @type {Object} */\n",
+					js => $extern_js,
+					deps => @deps
 				}
 			}
+			#decrement 
 			pop(@namespace);
 			$ns_len = scalar(@namespace);
 		}
-	}
-	#now reverse the namespace declarations so they are in the right order
-	@namespaceDecl = reverse(@namespaceDecl);
-	foreach $ns(@namespaceDecl){
-		print $ns;
+
+		#classes / constructors
+		if($type =~ /c/){
+				
+			if(!exists $externs{$link} && !is_external($link)){
+
+				push(@extern_jsdoc, "/**\n");
+				push(@extern_jsdoc, " * \@constructor\n");
+				if ($tagline =~ /inherits\:([^\t]+)\t/) {
+					push(@extern_jsdoc, " * \@extends {$1}\n");
+				}
+				#parse constructor function for parameters, args, and deps
+				%meta = getFnMeta($tagline);
+				$args = $meta{'args'};
+				foreach $param(@meta{'params'}){
+					push(@extern_jsdoc, $param);
+				}
+				push(@extern_jsdoc, " */\n");
+
+				#construct data object for this extern 
+				$externs{$link} = {
+					jsdoc => @extern_jsdoc,
+					js => "$link = function ($args) {};\n",
+					deps => @meta{'deps'}
+				};
+			}
+
+		} elsif($type =~ /m/){ #method
+
+			#methods have link == class which contains them
+			#attach methods to the prototype of the constructor,
+			#except static methods ( which are simply members of the 'class' object)
+			
+			if ($tagline =~ /\tisstatic\:yes/){
+				$extern = "$link\.$tagname";
+			} else {
+				#add method to constructor prototype
+				$extern = "$link\.prototype\.$tagname";
+			}
+
+			if(!exists $externs{$extern} && !is_external($tagname)){
+
+				push(@extern_jsdoc, "/**\n");
+				%meta = getFnMeta($tagline);
+				$args = $meta{'args'};
+
+				#if has type: use type as return
+				if($tagline =~ /\ttype\:([^\t]*)/){
+					$return = $1;
+					$return = convertType($return);
+					push(@extern_jsdoc, " * \@return {$return}\n");
+				}
+				push(@extern_jsdoc, " */\n");
+				$extern_js = "$extern = function($args){};\n");
+
+				#construct extern data object
+				$externs{$extern} = { jsdoc => @extern_jsdoc, js => $extern_js, deps => @meta{'deps'}};
+			}
+		
+		} elsif ($type =~ /v/){ #vars
+
+			$extern = "$link\.$tagname";
+			if(!exists $externs{$extern} && !is_external($tagname)){
+				#get type of var
+				if($tagline =~ /\ttype\:(\S*)/){
+					$type_spec = $1;
+					#get jsdoc style type declaration
+					$type_spec = convertType($type_spec);
+					push($extern_deps, $type_spec);
+					#get default value
+					$def_val = getDefVal($type_spec);
+					push(@extern_jsdoc, "/**\n");
+					push(@extern_jsdoc, " * \@type {$type_spec}\n");
+					push(@extern_jsdoc, " */\n");
+
+					if($def_val eq "undefined"){
+						$extern_js = "$extern;\n";
+					} else {
+						$extern_js = "$extern = $def_val;\n";
+					}
+				}
+			}
+			#construct extern object
+			$externs{$extern} = { jsdoc => @extern_jsdoc, js => $extern_js, deps => @meta{'deps'}};
+		}
 	}
 }
 
-#now print outlines
-foreach $outline(@outlines){
-	print $outline;
+# sort deps
+#TODO
+
+# output
+foreach $myextern(@externs){
+	
+	foreach $jsdoc_line(@myextern{'jsdoc'}){
+		print $jsdoc_line;
+	}
+	print $myextern{'js'};
+
+}
+
+sub getNSDeps
+{
+	my $namespace = shift;
+	my @ns_deps = ();
+	my $ns_dep = "";
+	#split on dots
+	@namespace = split(/\./, $namespace);
+	#lose last bit .. this is the method, class, or var itself
+	pop(@namespace)
+	$ns_len = scalar(@namespace);
+	$ns_idx = 0;
+	$ns_base = "";
+	while($ns_idx < $ns_len){
+		$ns_base .= $namespace[$ns_idx];
+		$ns_dep = "{$ns_base}";
+		push(@ns_deps, $ns_dep);
+		$ns_idx++;
+	}
+	return @ns_deps;
 }
 
 sub is_external
@@ -187,9 +220,12 @@ sub getFnMeta
 	my $pname;
 	my $ptype;
     my $args = "";
+	my @fn_params = ();
+	my @deps = ();
 
 	if($tag =~ /\tsignature\:\(([^\)]+)\)/){
 		$sig = $1;
+		#normalize parms into an array
 		if(length($sig) > 0){
 			if(index($sig, /\,/) > -1){
 				@params = split(/\,/, $sig);
@@ -201,6 +237,8 @@ sub getFnMeta
 			#no params
 			@params = ();
 		}
+
+		#iterate over the params.. parse name & type
 		foreach $param(@params){
 			#remove <++>
 			if($param =~ /\<\+([^\+]+)\+\>/){
@@ -218,8 +256,10 @@ sub getFnMeta
                         $pname =~ s/\*$// ;
                         $ptype = '...' . $ptype;
                     }
-					push(@outlines, " * \@param {$ptype} $pname \n");
-
+					#add param to array of parameter jsdoc declarations
+					push(@fn_params, " * \@param {$ptype} $pname \n");
+					#add the type declaration as a dependency
+					push(@fn_deps, $ptype);
                     #add pname to args
                     if ($args eq ""){
                         $args = $pname;
@@ -230,7 +270,7 @@ sub getFnMeta
 			}
 		}
 	}
-    return $args;
+    return { args => $args, params => @fn_params, deps => @deps};
 }
 sub convertType	
 {
